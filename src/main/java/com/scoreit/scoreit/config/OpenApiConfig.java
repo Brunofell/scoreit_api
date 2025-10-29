3790dcee993667895c587750906b4ec84cb3b94a
Brunofell <brunofelimarti@gmail.com>
2025-10-29 11:03:11 -0300

    swagger

---
+++ src/main/java/com/scoreit/scoreit/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.scoreit.scoreit.config;
+
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .info(new Info()
+                        .title("ScoreIt API")
+                        .description("API da rede social ScoreIt — avaliações de filmes, séries e álbuns.")
+                        .version("1.0.0"));
+    }
+
+}
